1517084f1e131c76a2df197499bd838386f22185
Caio Willian <caiow.willian@gmail.com>
2021-07-29 22:24:24 -0300

    chore: initial

--- src/app/app.go
+++ src/app/app.go
@@ -14,30 +14,52 @@ func Start() *cli.App {
 	app.Name = "app"
 	app.Usage = "find web servers"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "find address",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
-
-			Action: func(c *cli.Context) {
-				host := c.String("host")
-				ips, err := net.LookupIP(host)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				for _, ip := range ips {
-					fmt.Println(ip)
-				}
-			},
+			Name:   "ip",
+			Usage:  "find address",
+			Flags:  flags,
+			Action: findIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "find servers",
+			Flags:  flags,
+			Action: findServers,
 		},
 	}
 	return app
+}
+
+func findServers(c *cli.Context) {
+	host := c.String("host")
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
+}
+
+func findIps(c *cli.Context) {
+	host := c.String("host")
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
